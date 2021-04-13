6c2e3788ff425a7dd4f13b38c9e6d46e1fee1fc3
Noelle Notermann <noona020@umn.edu>
2021-04-13 04:46:46 +0000

    reordered server.py, updated html pages

--- server.py
+++ server.py
@@ -19,24 +19,6 @@ def homepage():
     return render_template('homepage.html')
 
 
-@app.route('/favorites')
-def all_favorites():
-    """View all favorited parks."""
-
-    favorites = crud.get_favorite_parks()
-
-    return render_template('all_favorited_parks.html', favorites=favorites)
-
-
-# @app.route('/favorites/<park_API_id>')
-# def show_movie(movie_id):
-#     """Show details on a particular movie."""
-
-#     movie = crud.get_movie_by_id(movie_id)
-
-#     return render_template('movie_details.html', movie=movie)
-
-
 @app.route('/users')
 def all_users():
     """View all user profiles."""
@@ -46,7 +28,35 @@ def all_users():
     return render_template('all_users.html', users=users)
 
 
-# @app.route('/users', methods=['POST'])
+@app.route('/login')
+def show_login_page(user_email):
+    """Show page for specific user with parks."""
+
+    # user = crud.get_user_by_email(user_email)
+    parks = crud.get_parks_by_user_zipcode(user_zipcode)
+
+    return render_template('user_login_page.html', parks=parks)
+
+
+@app.route('/login/<user_email>')
+def show_user_profile(user_email):
+    """Show profile of a specific user by email."""
+
+    user = crud.get_user_by_email(user_email)
+
+    return render_template('user_profile.html', user=user)
+
+
+@app.route('/favorites')
+def all_favorites():
+    """View all favorited parks."""
+
+    favorites = crud.get_favorite_parks()
+
+    return render_template('all_favorited_parks.html', favorites=favorites)
+
+
+# @app.route('/newaccount', methods=['POST'])
 # def create_user_profile():
 #     """Create a new user profile."""
 
@@ -63,13 +73,13 @@ def all_users():
 #     return redirect('/')
 
 
-@app.route('/users/<user_email>')
-def show_user_profile(user_email):
-    """Show profile of a specific user by email."""
+# @app.route('/favorites/<park_API_id>')
+# def show_movie(movie_id):
+#     """Show details on a particular movie."""
 
-    user = crud.get_user_by_email(user_email)
+#     movie = crud.get_movie_by_id(movie_id)
 
-    return render_template('user_profile.html', user=user)
+#     return render_template('movie_details.html', movie=movie)
 
 
 if __name__ == '__main__':
